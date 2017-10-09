a7438a4f33f53887bf4108dd08d602f77932fdbd
Seije <niels.gaastra@hotmail.com>
2017-10-09 23:16:15 +0200

    Update

--- detect_traffic.py
+++ detect_traffic.py
@@ -8,7 +8,8 @@ class Feed:
     def read(self):
         return self.feed.read()
 
-
+    def close(self):
+        return self.feed.close()
 class Cascade:
     def __init__(self, cascade):
         self.cascade = cv2.CascadeClassifier(cascade)
@@ -20,10 +21,39 @@ class Cascade:
 
 
 class Object:
-    def __init__(self, object_name, roi):
+    def __init__(self, roi, object_name = "Unknown"):
         self.object_name = object_name
         self.roi = roi
 
     def draw(self, frame):
-        cv2.rectangle(frame, (self.roi[0], self.roi[1]), (self.roi[2], self.roi[3]), (255, 0, 0), 2)
-        cv2.putText(frame, self.object_name, (self.roi[0], self.roi[1] - 20), "verdana", (0, 255, 255))
+        if self.roi is None:
+            print("ROI is not known")
+        else:
+            cv2.rectangle(frame, (self.roi[0], self.roi[1]), (self.roi[2], self.roi[3]), (255, 0, 0), 2)
+            cv2.putText(frame, self.object_name, (self.roi[0], self.roi[1] - 20), "verdana", (0, 255, 255))
+            return frame
+
+    def set_roi(self, roi):
+        self.roi = roi
+
+
+feed = Feed("img/warehouse.mp4")
+cascade = Cascade("cascades/haarcascade_fullbody.xml")
+
+objects = []
+
+while 1:
+    ret, frame = feed.read()
+    roi = cascade.get_objects(frame)
+    for obj in roi:
+        objects.append(Object(roi))
+
+    for obj in objects:
+        obj.draw(frame)
+
+
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+
+feed.close()
+cv2.destroyAllWindows()
