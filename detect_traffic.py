133085fe8eb9ae5a8df8dfeae0f5734f4688103c
NGaastra <ntgaastr@student.avans.nl>
2017-10-16 12:57:26 +0200

    Fixed problem where position isnt updated when known object is found

--- detect_traffic.py
+++ detect_traffic.py
@@ -5,9 +5,8 @@ import imutils
 
 
 #PROBLEMS ATM
-#Region of interest not being updated when calling tracker
-#Maybe something wrong with drawing staying on screen (Probably from problem 1)
-
+#Region of interest not being updated when calling tracker!!! (Tracker problem? Problem in code? POINTER PROBLEM!?)
+#Region of interest is updated while known object is found and then changed back to original position when tracker is updated
 
 
 class Feed:
@@ -29,7 +28,7 @@ class Cascade:
 
     def get_objects(self, frame):
         gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-        objects = self.cascade.detectMultiScale(gray, 1.3, 5)
+        objects = self.cascade.detectMultiScale(gray, 1.1, 2)
         return objects
 
 
@@ -37,19 +36,17 @@ class Object:
     def __init__(self, frame, roi, object_name = "Unknown"):
         self.tracker = Tracker(frame, roi)
         self.roi = roi
-        self.p1, self.p2 = (roi[0], roi[1]), (roi[0] + roi[2], roi[1]+ roi[3])
         self.object_name = object_name
         self.frame = frame
 
     def draw(self, frame):
-        cv2.rectangle(frame, self.p1, self.p2, (255, 0, 0), 2)
-        cv2.putText(frame, self.object_name, (self.p1[0], self.p1[1] - 5), cv2.FONT_HERSHEY_COMPLEX, 1, (0, 255, 255), 1)
+        cv2.rectangle(frame, (int(self.roi[0]), int(self.roi[1])), (int(self.roi[0] + self.roi[2]), int(self.roi[1]+ self.roi[3])), (255, 0, 0), 2)
+        cv2.putText(frame, self.object_name, (int(self.roi[0]), int(self.roi[1]) - 5), cv2.FONT_HERSHEY_COMPLEX, 1, (0, 255, 255), 1)
 
     def set_roi(self, roi):
         self.roi = roi
 
     def update_tracker(self):
-        print(self.roi)
         ok, self.roi = self.tracker.update(self.frame)
         return ok
 
@@ -58,13 +55,12 @@ class Tracker:
         self.init, self.tracker = self.init_tracker(frame, roi)
 
     def init_tracker(self, frame, roi):
-        tracker = cv2.TrackerMedianFlow_create()
+        tracker = cv2.TrackerKCF_create()
         ret = tracker.init(frame, tuple(roi))
         return ret, tracker
 
     def update(self, frame):
         ret, roi = self.tracker.update(frame)
-        print(ret, " - ", roi, "\n")
         return ret, roi
 
 
@@ -79,7 +75,7 @@ class Traffic:
 
     def update_bodies(self, frame):
         updated = self.get_bodies(frame)
-        self.update_trackers(updated)
+        #self.update_trackers(updated)
 
     def update_trackers(self, updated):
         for i, obj in enumerate(self.traffic):
@@ -100,56 +96,50 @@ class Traffic:
             for body in bodies:
 
                 #Check if body already exists
-                index = self.get_object(body, 5)
-                #print(body)
+                index = self.get_object(body, 70)
                 if index == -1: #Found new body
-                    self.traffic.append(Object(frame, body, "Person"))
+                    self.traffic.append(Object(frame, tuple(body), "Person"))
                     updated.append(len(self.traffic) - 1) #Add just added value from traffic list to updated list
                 else: #Found known body
-                    #print("Index ", index, " Old: ", self.traffic[index].roi, "  New: ", body)
-                    self.traffic[index].roi = body
-                    #print(self.traffic[index].roi)
+                    self.traffic[index].set_roi(tuple(body))
         return updated
 
     def draw_traffic(self, frame):
-        for obj in self.traffic:
+        for i, obj in enumerate(self.traffic):
             obj.draw(frame)
 
     def set_feed(self, feed):
         self.feed = Feed(feed)
 
-    def get_dist(self, object1, object2):
-        x1, y1 = object1[0], object1[1]
-        x2, y2 = object2[0], object2[1]
+    def get_dist(self, obj1, obj2):
+        #Get x, y from object 1
+        x1, y1 = obj1[0], obj1[1]
+
+        #Get x, y from object 2
+        x2, y2 = obj2[0], obj2[1]
+
+        #Get distance between objects
         ans = int(math.hypot(x2 - x1, y2 - y1))
         return ans
 
     def get_object(self, roi, margin):
-        for i, obj2 in enumerate(self.traffic):
-            if self.get_dist(roi, obj2.roi) < margin:
+        for i, obj in enumerate(self.traffic):
+            if self.get_dist(roi, obj.roi) < margin:
                 return i
         return -1
 
     def find_traffic(self):
-        obj = []
-        person = None
         while 1:
             ret, frame = self.feed.read()
-            #self.update_bodies(frame)
-            #self.draw_traffic(frame)
-            if len(obj) is 0:
-                obj = self.body_cascade.get_objects(frame)
-                for obj1 in obj:
-                    person = Object(frame, obj1)
+            self.update_bodies(frame)
+            self.draw_traffic(frame)
 
-            if len(obj) is not 0:
-                person.update_tracker()
             cv2.imshow('Warehouse', frame)
             k = cv2.waitKey(1) & 0xFF
             if k == 27:
                 break
 
-traffic = Traffic("img/walking1.mp4")
+traffic = Traffic("img/walking4.mp4")
 traffic.set_body_cascade("cascades/haarcascade_fullbody.xml")
 
 traffic.find_traffic()
