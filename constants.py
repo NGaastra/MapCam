2a2bc08312585bf2c72d87f6d63f2d3bbd92d7f3
NGaastra <ntgaastr@student.avans.nl>
2017-10-18 12:04:43 +0200

    Added search region reduction (not quite finished)

--- constants.py
+++ constants.py
@@ -1,6 +1,11 @@
-CAMERA_ANGLE = 60               # Degrees
-CAMERA_HEIGHT = 30              # Meters
-CAMERA_VIEWING_ANGLE = 50       # Degrees (Total)
+CAMERA_ANGLE = 60                       # Degrees
+CAMERA_HEIGHT = 30                      # Meters
+CAMERA_VIEWING_ANGLE = 50               # Degrees (Total)
 
-FEED_WIDTH = 640                 # Pixels
-FEED_HEIGHT = 480               # Pixels
+FEED_WIDTH = 640                        # Pixels
+FEED_HEIGHT = 480                       # Pixels
+
+SEARCH_MARGIN_NEW_LOC = 50              # Pixels
+
+RECTANGLE_COLOR_PERSON = (255, 0, 0)    # BGR
+TEXT_COLOR = (0, 255, 255)              # BGR
--- detect_traffic.py
+++ detect_traffic.py
@@ -11,8 +11,10 @@ import constants
 # None
 
 # TODO
-# Haar cascade for forklift
-# Position fetching
+# Haar cascade for other traffic
+# Searching in local region of ROI for new location cascade search
+# Make seperate frame copy for drawing, so it doesnt interfere with haar features
+# Add comments
 
 
 class Feed:
@@ -21,8 +23,8 @@ class Feed:
 
     def read(self):
         ret, read = self.feed.read()
-        crop = imutils.resize(read, width=constants.FEED_WIDTH, height=constants.FEED_HEIGHT)
-        return ret, crop
+        resized = imutils.resize(read, width=constants.FEED_WIDTH, height=constants.FEED_HEIGHT)
+        return ret, resized
 
     def set_feed(self, feed):
         self.close()
@@ -37,10 +39,26 @@ class Cascade:
         self.cascade = cv2.CascadeClassifier(cascade)
 
     def get_objects(self, frame):
+        # Convert frame to grayscale
         gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+
+        # Search grayscale frame for cascade match
         objects = self.cascade.detectMultiScale(gray, 1.1, 4)
         return objects
 
+    def get_new_location(self, frame, obj):
+        # Convert frame to grayscale
+        gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+
+        # Crop grayscale frame to ROI + margin size
+        gray_crop = gray[int(obj.roi[1]) - constants.SEARCH_MARGIN_NEW_LOC: int(obj.roi[1] + obj.roi[3]) + constants.SEARCH_MARGIN_NEW_LOC,
+                             int(obj.roi[0]) - constants.SEARCH_MARGIN_NEW_LOC: int(obj.roi[0] + obj.roi[2]) + constants.SEARCH_MARGIN_NEW_LOC]
+
+        cv2.imshow('search', gray_crop)
+
+        # Search in this cropped area for cascade match
+        objects = self.cascade.detectMultiScale(gray_crop, 1.1, 4)
+        return objects
 
 class Object:
     def __init__(self, frame, roi, object_name = "Unknown"):
@@ -49,22 +67,34 @@ class Object:
         self.object_name = object_name
 
     def draw(self, frame):
+        # ROI is not initialized
         if self.roi is None:
             print("ROI is not known")
+        # ROI is initialized
         else:
-            cv2.rectangle(frame, (int(self.roi[0]), int(self.roi[1])), (int(self.roi[0] + self.roi[2]), int(self.roi[1] + self.roi[3])), (255, 0, 0), 2)
-            cv2.putText(frame, self.object_name, (int(self.roi[0]), int(self.roi[1]) - 5), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 255), 1)
+            # Draw rectangle to match ROI
+            cv2.rectangle(frame, (int(self.roi[0]), int(self.roi[1])), (int(self.roi[0] + self.roi[2]), int(self.roi[1] + self.roi[3])), constants.RECTANGLE_COLOR_PERSON, 2)
+
+            # Put object name above the object
+            cv2.putText(frame, self.object_name, (int(self.roi[0]), int(self.roi[1]) - 5), cv2.FONT_HERSHEY_SIMPLEX, 1, constants.TEXT_COLOR, 1)
             return frame
 
     def set_roi(self, roi):
+        # Set ROI
         self.roi = roi
+
+        # Update bottom position with new ROI
         self.bottom = (roi[0] + (roi[2] / 2), roi[1] + roi[3])
 
     def init_tracker(self, frame, roi):
+        # Initialize tracker
         self.tracker.init_tracker(frame, roi)
 
     def update_tracker(self, frame):
+        # Update tracker
         ok, roi = self.tracker.update(frame)
+
+        # Set ROI to new ROI determined by the tracker
         self.set_roi(roi)
         return ok
 
@@ -74,11 +104,14 @@ class Tracker:
         self.init = self.init_tracker(frame, roi)
 
     def init_tracker(self, frame, roi):
+        # Initialize tracker
         ret = self.tracker.init(frame, roi)
         return ret
 
     def update(self, frame):
+        # Tracker is initialized
         if self.init:
+            # Update tracker
             ret, roi = self.tracker.update(frame)
             return ret, roi
 
@@ -92,13 +125,18 @@ class Corridor:
         self.corr_end_p1, self.corr_end_p2 = None, None
 
     def set_feed(self, feed):
+        # Set feed
         self.feed.set_feed(feed)
 
     def map(self, x, in_min, in_max, out_min, out_max):
+        # Map range of values to new range of values
         return (x - in_min) * (out_max - out_min) / (in_max - in_min) + out_min
 
     def get_depth(self, y):
+        # Calculate angle that corresponds with the current position of the object
         angle = self.map(y, 0, constants.FEED_HEIGHT, (constants.CAMERA_ANGLE + (constants.CAMERA_VIEWING_ANGLE / 2)), (constants.CAMERA_ANGLE - (constants.CAMERA_VIEWING_ANGLE / 2)))
+
+        # Calculate distance based on camera height and camera angle
         dist = constants.CAMERA_HEIGHT * math.tan(math.radians(angle))
         return dist
 
@@ -176,6 +214,7 @@ class Corridor:
             _, frame = self.feed.read()
             self.traffic.find_traffic(frame)
             for obj in self.traffic.traffic:
+                self.traffic.body_cascade.get_new_location(frame, obj)
                 print(obj.object_name, " -- ", self.get_position(obj))
             cv2.imshow('Warehouse', frame)
             k = cv2.waitKey(1) & 0xFF
@@ -213,13 +252,17 @@ class Traffic:
             for body in bodies:
 
                 #Check if body already exists
-                index = self.get_object(body, 200)
-                if index == -1: #Found new body
+                index = self.get_object(body)
+                # Found new body
+                if index == -1:
                     self.traffic.append(Object(frame, tuple(body), "Person"))
                     updated.append(len(self.traffic) - 1) #Add just added value from traffic list to updated list
-                else: #Found known body
+                # Found known body
+                else:
                     print("Reinit - ", body)
+                    # Set new ROI
                     self.traffic[index].set_roi(tuple(body))
+                    # Update tracker initialization to match new ROI
                     self.traffic[index].init_tracker(frame, tuple(body))
         return updated
 
@@ -238,9 +281,12 @@ class Traffic:
         ans = int(math.hypot(x2 - x1, y2 - y1))
         return ans
 
-    def get_object(self, roi, margin):
+    def get_object(self, roi):
+        # Iterate through every object in traffic
         for i, obj in enumerate(self.traffic):
-            if self.get_dist(roi, obj.roi) < margin:
+            # If distance is smaller than given SEARCH_MARGIN_NEW_LOC
+            if self.get_dist(roi, obj.roi) < constants.SEARCH_MARGIN_NEW_LOC:
+                # Return index
                 return i
         return -1
 
